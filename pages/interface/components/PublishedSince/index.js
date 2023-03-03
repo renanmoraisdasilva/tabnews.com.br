c98b236ef460625d45a264386c1c996414396742
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2023-03-03 19:58:05 +0000

    fix(date tooltip): fix client time zone

--- pages/interface/components/PublishedSince/index.js
+++ pages/interface/components/PublishedSince/index.js
@@ -1,6 +1,7 @@
 import { formatDistanceToNowStrict, format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { Tooltip } from '@primer/react';
+import { useEffect, useState } from 'react';
 
 function formatPublishedSince(date) {
   const publishedSince = formatDistanceToNowStrict(new Date(date), {
@@ -15,8 +16,16 @@ function formatTooltipLabel(date) {
 }
 
 export default function PublishedSince({ date }) {
+  // added invisible character to force rendering the correct time on the client
+  // https://github.com/filipedeschamps/tabnews.com.br/pull/1294#pullrequestreview-1324466790
+  const [tooltipLabel, setTooltipLabel] = useState('\u034f' + formatTooltipLabel(date));
+
+  useEffect(() => {
+    setTooltipLabel(formatTooltipLabel(date));
+  }, [date]);
+
   return (
-    <Tooltip sx={{ position: 'absolute', ml: 1 }} aria-label={formatTooltipLabel(date)}>
+    <Tooltip suppressHydrationWarning sx={{ position: 'absolute', ml: 1 }} aria-label={tooltipLabel}>
       <span suppressHydrationWarning>{formatPublishedSince(date)}</span>
     </Tooltip>
   );
